26a37ae7fadf3a9a14c2d43376f3854703c0721d
Emily Jackson <emj@Emilys-MacBook-Air.local>
2021-03-25 21:17:40 -0400

    new stuff ya

--- day_2_notes.js
+++ day_2_notes.js
@@ -5,3 +5,7 @@
 //once committed your branch will come back as working tree clean
 //git log = shows what commits you've made with commit hash, branch name, commit msg, and other metadata
 // to commit your changes =  git commit -m you message here
+//how to connect a repo from github - create repo in github and copy uri then place: 
+//git remote add origin https://github.com/emnojacks/class_notes.git
+//git remote -v = shows if it is indeed linked
+//to push to remote repo git push origin master
