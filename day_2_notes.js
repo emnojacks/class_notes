dcf31bfb270dd4d5848038c348114a3b7d98d695
Emily Jackson <emj@Emilys-MacBook-Air.local>
2021-03-25 20:44:24 -0400

    This is my first push to git

---
+++ day_2_notes.js
@@ -0,0 +1,4 @@
+//to initiate git repo in files = git init 
+// staging files for a commit= git status
+//git add <file name> OR git add . to add all of the files inside the folder
+//git commit -m "your commit message here"
