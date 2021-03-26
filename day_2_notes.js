3f49d3f41af126caaa367786ed3a4bdd0267466d
Emily Jackson <emj@Emilys-MacBook-Air.local>
2021-03-25 20:56:48 -0400

    added a few more comments on git

--- day_2_notes.js
+++ day_2_notes.js
@@ -1,4 +1,7 @@
 //to initiate git repo in files = git init 
 // staging files for a commit= git status
 //git add <file name> OR git add . to add all of the files inside the folder
-//git commit -m "your commit message here"
+//git commit -m "your commit message here"//
+//once committed your branch will come back as working tree clean
+//git log = shows what commits you've made with commit hash, branch name, commit msg, and other metadata
+// to commit your changes =  git commit -m you message here
